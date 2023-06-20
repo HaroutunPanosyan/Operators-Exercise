63f3a84f2b383d2559db6bceffab51d375282471
Haroutun Panosyan <harupanosyan@gmail.com>
2023-06-20 12:37:36 -0700

    Finished Both Exercises

---
+++ OperatorExercise/AreaOfCircle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    public class AreaOfCircle
+    {
+        public static double Calculate(double radius)
+        {
+            double formula = Math.PI * Math.Pow(radius, 2);
+            return formula;
+        }
+    }
+}
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1
+
+            //-------------------------------
+
+            Console.WriteLine(5 + 2);
+            Console.WriteLine(5 - 2);
+            Console.WriteLine(6 / 2);
+            Console.WriteLine(5 * 2);
+
+            int a = 17;
+            int b = 4;
+            int quotient = a % b;
+            Console.WriteLine($" The remainder of {a}/{b} is {quotient}.");
+
+            //-------------------------------
+
+            //Exercise 2
+
+            //-------------------------------
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(AreaOfCircle.Calculate(radius));
+
         }
     }
 }
